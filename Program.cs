dfc0c5d3bc993ce4ec73eaac257f6fffaa907a50
jihunroh <jihunroh@outlook.kr>
2018-05-07 18:45:34 +0900

    계산 소요시간 오류 수정

--- Program.cs
+++ Program.cs
@@ -8,20 +8,18 @@ namespace ProjectEuler
         static void Main(string[] args)
         {
             var watch = System.Diagnostics.Stopwatch.StartNew();
-            var elapsedMs = watch.ElapsedMilliseconds;
 
-            int problemNumber = 2;
-            int problemSolution;
+            int problemNumber = 14;
+            long problemSolution;
 
-            problemSolution = Problem002.Solution();
+            problemSolution = (long) Problem014.Solution();
             watch.Stop();
 
             Console.WriteLine("------------------------------------------------");
             Console.WriteLine("   Project Euler: Problem #{0}", problemNumber);
             Console.WriteLine("   The Answer is: {0}", problemSolution);
-            Console.WriteLine("   Time Elasped: {0}ms", elapsedMs);
+            Console.WriteLine("   Time Elasped: {0}ms", watch.ElapsedMilliseconds);
             Console.WriteLine("------------------------------------------------");
-            //Console.ReadKey();
             Console.Read();
         }
     }
